63127ef9b89319a4aef2af90ca8b4097ca420999
xzhang2523 <xzhang2523-c@my.cityu.edu.hk>
2024-08-15 23:02:27 +0800

    Update indicators

--- libmoon/tester/run_syn.sh
+++ libmoon/tester/run_syn.sh
@@ -2,20 +2,18 @@
 niter=2000
 
 
-for seed in 0 1 2 3 4
+for seed in 0
   do
   for problem_name in VLMOP1 VLMOP2
   do
-      for solver in pmgda epo
+      for solver in epo
       do
         python run_syn_clean.py --solver $solver --seed $seed --problem-name $problem_name --n-iter $niter
       done
-
-      for agg in mtche cosmos
-      do
-        python run_syn_clean.py --solver agg --seed $seed --problem-name $problem_name --agg $agg --n-iter $niter
-      done
-    done
+  done
 done
+
+
+
 
 sleep 100
--- libmoon/tester/run_syn_clean.py
+++ libmoon/tester/run_syn_clean.py
@@ -1,5 +1,7 @@
 import argparse
 import numpy as np
+import sys
+sys.path.append('D:\\pycharm_project\\libmoon\\')
 from libmoon.util_global import synthetic_init, get_problem, uniform_pref
 from libmoon.solver.gradient.methods import EPOSolver
 from libmoon.solver.gradient.methods.pmgda_solver import PMGDASolver
@@ -8,11 +10,12 @@ import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 from matplotlib import pyplot as plt
 from libmoon.util_global.constant import FONT_SIZE_2D, FONT_SIZE_3D, color_arr, beautiful_dict, root_name
+from util_global.constant import plt_2d_pickle_size, plt_2d_marker_size
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser( description= 'example script' )
-    # Inv agg not very work
     parser.add_argument( '--agg', type=str, default='mtche')  # If solve is agg, then choose a specific agg method.
     parser.add_argument('--solver', type=str, default='agg')
     parser.add_argument( '--problem-name', type=str, default='VLMOP1')
@@ -20,14 +23,14 @@ if __name__ == '__main__':
     parser.add_argument('--tol', type=float, default=1e-2)
     parser.add_argument('--plt-pref-flag', type=str, default='N')
     parser.add_argument('--use-plt', type=str, default='Y')
-    parser.add_argument('--PaperName', type=str, default='TETCI')
-    # For PMGDA.
+    parser.add_argument('--PaperName', type=str, default='Output')
     parser.add_argument('--h-tol', type=float, default=1e-3)
     parser.add_argument('--sigma', type=float, default=0.9)
     parser.add_argument('--n-prob', type=int, default=8 )
-    parser.add_argument('--n-iter', type=int, default=2000 )
+    parser.add_argument('--n-iter', type=int, default=10 )
     parser.add_argument('--seed-idx', type=int, default=0)
     args = parser.parse_args()
+
 
     if args.solver=='agg':
         args.task_name = '{}_{}'.format(args.solver, args.agg)
@@ -37,8 +40,6 @@ if __name__ == '__main__':
     print('Problem: {}'.format(args.problem_name))
     print('Task Name: {}'.format(args.task_name ))
     print('Seed: {}'.format(args.seed_idx))
-
-
 
     hv_seed = []
     seed_num = 3
@@ -50,7 +51,6 @@ if __name__ == '__main__':
         args.n_prob = 15
 
     prefs = uniform_pref(n_prob=args.n_prob, n_obj = problem.n_obj, clip_eps=1e-2)
-
     if args.solver == 'epo':
         solver = EPOSolver(problem, step_size=1e-2, n_iter=args.n_iter, tol=args.tol )
     elif args.solver == 'pmgda':
@@ -58,18 +58,21 @@ if __name__ == '__main__':
     elif args.solver == 'agg':
         solver = GradAggSolver(problem, step_size=1e-2, n_iter=args.n_iter, tol=args.tol, agg=args.agg)
 
-
     res = solver.solve( x=synthetic_init(problem, prefs), prefs=prefs )
     res['pref_mat'] = prefs
-    # hv_seed.append( res['hv_history'] )
     sub_sample=1
     if problem.n_obj == 2:
-        for idx in range(len(prefs)):
-            plt.plot(res['y_history'][::sub_sample, idx, 0], res['y_history'][::sub_sample, idx, 1],
-                    color=color_arr[idx])
+        y_arr = res['y']
+        plt.scatter(y_arr[:, 0], y_arr[:, 1], color='black')
+        plt.xticks(fontsize=plt_2d_pickle_size)
+        plt.yticks(fontsize=plt_2d_pickle_size)
+
+        plt.xlabel('$L_1$', fontsize=plt_2d_marker_size)
+        plt.ylabel('$L_2$', fontsize=plt_2d_marker_size)
+
     elif problem.n_obj == 3:
         ax = (plt.figure()).add_subplot(projection='3d')
-        for idx in range( len(prefs) ) :
+        for idx in range(len(prefs)):
             ax.plot(res['y_history'][::sub_sample, idx, 0], res['y_history'][::sub_sample, idx, 1], res['y_history'][::sub_sample, idx, 2],
                     color=color_arr[idx])
         prefs_l2 = prefs / np.linalg.norm(prefs, axis=1, keepdims=True)
@@ -81,18 +84,15 @@ if __name__ == '__main__':
         x = np.cos(theta) * np.sin(phi)
         y = np.sin(theta) * np.sin(phi)
         z = np.cos(phi)
-
         ax.plot_surface(x, y, z, alpha=0.3)
         ax.axis('equal')
         ax.view_init(30, 45)
         ax.set_xlim([0, 1.2])
         ax.set_ylim([0, 1.2])
         ax.set_zlim([0, 1.2])
-
         ax.set_xlabel('$L_1$', fontsize=FONT_SIZE_3D)
         ax.set_ylabel('$L_2$', fontsize=FONT_SIZE_3D)
         ax.set_zlabel('$L_3$', fontsize=FONT_SIZE_3D)
-
 
     fig_folder_name = os.path.join(root_name, args.PaperName, args.problem_name, '{}'.format(args.seed_idx))
     os.makedirs(fig_folder_name, exist_ok=True)

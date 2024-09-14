dc2fcd4246a047ad4ebeb9efcf523150ae9b7aa2
xzhang2523 <xzhang2523-c@my.cityu.edu.hk>
2024-09-14 20:09:03 +0800

    Update .gitignore

--- libmoon/problem/synthetic/ml_problem.py
+++ libmoon/problem/synthetic/ml_problem.py
@@ -1,12 +1,18 @@
+import matplotlib.pyplot as plt
+
 from libmoon.problem.synthetic.mop import BaseMOP
 import torch
 import numpy as np
 from torch import Tensor
 from torch import nn
 
+
+
+import argparse
 '''
-    Adpats from: Hu et al. Revisiting Scalarization in Multi-Task Learning:
+    (1) Regression, adpats from: Hu et al. Revisiting Scalarization in Multi-Task Learning:
             A Theoretical Perspective. NeurIPS 2023. 
+    (2) MODM, adapte from: . 
 '''
 
 class VirationalInference():
@@ -45,41 +51,65 @@ class DivergenceMacthing(BaseMOP):
 class MODM(BaseMOP):
     def __init__(self, n_var=5, n_obj=2, mu1=None, Sigma1=None,
                          mu2=None, Sigma2=None):
+        '''
+            Description:
+                MODM problem. KL divergence matching between two multi-variate Gaussian distributions.
+        '''
         super().__init__(n_var=n_var, n_obj=n_obj)
-        self.mu1 = torch.zeros(n_var)
-        self.Sigma1 = torch.eye(n_var)
 
-        self.mu2 = torch.ones(n_var)
-        self.Sigma2 = torch.eye(n_var)
+        if mu1 is None:
+            mu1 = torch.zeros(n_var)
+            Sigma1 = torch.eye(n_var)
+        if mu2 is None:
+            mu2 = torch.ones(n_var)
+            Sigma2 = torch.eye(n_var)
 
-        self.dist1 = torch.distributions.MultivariateNormal(mu1, Sigma1)
-        self.dist2 = torch.distributions.MultivariateNormal(mu2, Sigma2)
+
+        self.mu1 = mu1
+        self.Sigma1 = Sigma1
+
+        self.mu2 = mu2
+        self.Sigma2 = Sigma2
+
+        self.dist1 = torch.distributions.MultivariateNormal(self.mu1, self.Sigma1)
+        self.dist2 = torch.distributions.MultivariateNormal(self.mu2, self.Sigma2)
         self.problem_name = 'modm'
 
-
     def _evaluate_torch(self, x):
+        '''
+            Input: x: torch.Tensor, shape: (n_prob, n_var)
+            Output: torch.Tensor, shape: (n_prob, n_obj)
+        '''
+        n_prob, n_var = x.shape
         Sigma = torch.eye(self.n_var)
-        dist = torch.distributions.MultivariateNormal(x, Sigma)
-        kl1 = torch.distributions.kl.kl_divergence(self.dist1, dist)
-        # result_arr = []
-        # for idx in range(n_prob):
-        #     dist = torch.distributions.Normal(x[idx], 1)
-        #     kl1 = torch.distributions.kl.kl_divergence(self.dist1, dist)
-        #     kl2 = torch.distributions.kl.kl_divergence(self.dist2, dist)
-        #     result = torch.stack((kl1, kl2), dim=1)
-        #     result_arr.append(result.squeeze())
-        # res = torch.stack(result_arr)
-        # return res
+
+        kl_arr = []
+        for prob_idx in range(n_prob):
+            x_i = x[prob_idx]
+            dist = torch.distributions.MultivariateNormal(x_i, Sigma)
+            kl1 = torch.distributions.kl.kl_divergence(dist, self.dist1)
+            kl2 = torch.distributions.kl.kl_divergence(dist, self.dist2)
+            kl_arr.append(torch.stack([kl1, kl2]))
+        return torch.stack(kl_arr)
+
 
     def _get_pf(self, n_points: int = 100):
-        mu_arr = np.linspace(0, 1, n_points)
+        mu_10 = self.mu1[0].numpy()
+        mu_20 = self.mu2[0].numpy()
+
+        mu_idx = np.linspace(mu_10, mu_20, n_points)
+        mu = torch.repeat(mu_idx, self.n_var, 1).T
+
         div_arr = []
-        for mu in mu_arr:
-            dist = torch.distributions.Normal(mu, 1)
-            kl_div1 = torch.distributions.kl.kl_divergence(self.dist1, dist)
-            kl_div2 = torch.distributions.kl.kl_divergence(self.dist2, dist)
+        Sigma = torch.eye(self.n_var)
+        for mu_i in mu:
+            dist = torch.distributions.MultivariateNormal(mu_i, Sigma)
+            kl_div1 = torch.distributions.kl.kl_divergence(dist, self.dist1)
+            kl_div2 = torch.distributions.kl.kl_divergence(dist, self.dist2)
             div_arr.append([float(kl_div1.numpy()), float(kl_div2.numpy())])
         return np.array(div_arr)
+
+
 
 class LinearRegreesion(BaseMOP):
     def __init__(self, n_var=5, n_obj=2):
@@ -90,7 +120,6 @@ class LinearRegreesion(BaseMOP):
                          lbound=lbound,
                          ubound=ubound)
         # k : n_obj. k regression tasks.
-        # n is the sample numble
         self.problem_name = 'regression'
         self.p = 5
         self.q = n_var // self.p
@@ -109,7 +138,6 @@ class LinearRegreesion(BaseMOP):
         for prob_idx in range(n_prob):
             W_idx = W_[prob_idx,:,:]
             Z = (self.X @ W_idx)
-            # Z.shape: (n, q)
             Proj = Z @ torch.linalg.pinv(Z.T @ Z) @ Z.T
             loss_arr = []
             for k in range(self.n_obj):
@@ -165,9 +193,18 @@ class NNRegression(nn.Module):
         return self.X, self.Y
 
 
-
 if __name__ == '__main__':
-    n_var = 5
-    problem = MODM(n_var=n_var)
-    x = torch.rand(2, n_var)
-    print(problem.evaluate(x))
+    parser = argparse.ArgumentParser(description='example script')
+    parser.add_argument('--n-var', type=int, default=5)
+    parser.add_argument('--n-prob', type=int, default=10)
+
+    args = parser.parse_args()
+    problem = MODM(n_var=args.n_var)
+    x = torch.rand(args.n_prob, args.n_var)
+    obj = problem.evaluate(x)
+
+    pf = problem._get_pf()
+
+    plt.scatter(pf[:,0], pf[:,1])
+    plt.show()
+    print()

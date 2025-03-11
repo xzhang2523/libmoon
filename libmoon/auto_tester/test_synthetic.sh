803035edf305f919624cadd9e767cbd563c698b6
xzhang2523 <xzhang2523-c@my.cityu.edu.hk>
2025-03-11 20:34:02 +0800

    CI

--- libmoon/auto_tester/test_synthetic.sh
+++ libmoon/auto_tester/test_synthetic.sh
@@ -1,12 +1,5 @@
-#for solver in PMGDA EPO MOOSVGD GradHV PMTL GradAgg MGDAUB
-##do
-##  python libmoon/auto_tester/test_synthetic.py --solver-name $solver --n-epoch 2000
-##done
-#do
-#  python test_synthetic.py --solver-name $solver --n-epoch 20000
-#done
-
 nepoch=1
+
 for agg in STche
 do
   python manual_tester/test_synthetic.py --solver-name GradAgg --agg-name $agg --n-epoch $nepoch

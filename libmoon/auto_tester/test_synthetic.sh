c8d6eea1ea18da7731ba7ca61f1134342808a9cc
xzhang2523 <xzhang2523-c@my.cityu.edu.hk>
2025-03-11 20:37:52 +0800

    CI

--- libmoon/auto_tester/test_synthetic.sh
+++ libmoon/auto_tester/test_synthetic.sh
@@ -2,17 +2,17 @@ nepoch=1
 
 for agg in STche
 do
-  python manual_tester/test_synthetic.py --solver-name GradAgg --agg-name $agg --n-epoch $nepoch
+  python auto_tester/test_synthetic.py --solver-name GradAgg --agg-name $agg --n-epoch $nepoch
 done
 
 for solver in UMOD
 do
-  python manual_tester/test_synthetic.py --solver-name $solver --agg-name STche --n-epoch $nepoch
+  python auto_tester/test_synthetic.py --solver-name $solver --agg-name STche --n-epoch $nepoch
 done
 
 for solver in PMGDA EPO MOOSVGD GradHV PMTL MGDAUB
 do
-  python manual_tester/test_synthetic.py --solver-name $solver --agg-name STche --n-epoch $nepoch
+  python auto_tester/test_synthetic.py --solver-name $solver --agg-name STche --n-epoch $nepoch
 done
 
 
